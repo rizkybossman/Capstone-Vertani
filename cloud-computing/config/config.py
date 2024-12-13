ddc36ad1785e74f6ad28b77f2a4279c048e51cff
Rizky Fauzi <114433481+rizkybossman@users.noreply.github.com>
2024-12-13 12:49:22 +0700

    Delete cloud-computing directory

--- cloud-computing/config/config.py
+++
@@ -1,4 +0,0 @@
-
-
-FIREBASE_CREDENTIALS = "service account"  # Path to your Firebase service account key
-MODEL_PATH = 'soil_detection_model.h5'  # Path to your model
--- cloud-computing/config/firebase_setup.py
+++
@@ -1,7 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
-
-# Inisialisasi Firebase
-cred = credentials.Certificate("vertani-443915-2f6f5f668cea.json")  # Ganti path file
-firebase_admin.initialize_app(cred)
-db = firestore.client()
